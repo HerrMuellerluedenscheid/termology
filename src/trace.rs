4654d5c80c870ff2f154937e1e1b7754b3c7bf17
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2021-05-18 22:52:55 +0200

    load miniseed

--- src/trace.rs
+++ src/trace.rs
@@ -1,7 +1,18 @@
-// file handling
-use libmseed_sys::ms_record;
+use miniseed::ms_record;
 
-pub fn read_mseed(file: &str) {
-    let m = ms_record::read(file);
-    println!("...... record {}", m);
+pub fn read_mseed(file: &str) -> Vec<i32> {
+    let rec = ms_record::read(file);
+    let data = rec.data_i32().to_owned();
+    data
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::trace::read_mseed;
+
+    #[test]
+    fn load_mseed() {
+        let file = "../libmseed-sys/tests/sample.miniseed";
+        read_mseed(&file);
+    }
 }
